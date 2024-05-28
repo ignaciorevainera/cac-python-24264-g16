998ba859cc4e45a21dfb209bf654f81eb3561b0f
Estefania Gauto <ingestefaniagauto@gmail.com>
2024-05-27 21:43:09 -0300

    borra lineas de js

---
+++ pages/serviciosJ.js
@@ -0,0 +1,43 @@
+const btnAbrirModal1 = document.querySelector("#btn-abrir-modal1");
+const btnCerrarModal1 = document.querySelector("#btn-cerrar-modal1");
+const modal1 = document.querySelector("#modal1");
+
+btnAbrirModal1.addEventListener("click",()=>{modal1.showModal();
+
+})
+btnCerrarModal1.addEventListener("click",()=>{modal1.close();
+
+})
+
+const btnAbrirModal2 = document.querySelector("#btn-abrir-modal2");
+const btnCerrarModal2 = document.querySelector("#btn-cerrar-modal2");
+const modal2 = document.querySelector("#modal2");
+
+btnAbrirModal2.addEventListener("click",()=>{modal2.showModal();
+
+})
+btnCerrarModal2.addEventListener("click",()=>{modal2.close();
+    
+})
+
+const btnAbrirModal3 = document.querySelector("#btn-abrir-modal3");
+const btnCerrarModal3 = document.querySelector("#btn-cerrar-modal3");
+const modal3 = document.querySelector("#modal3");
+
+btnAbrirModal3.addEventListener("click",()=>{modal3.showModal();
+
+})
+btnCerrarModal3.addEventListener("click",()=>{modal3.close();
+    
+})
+
+const btnAbrirModal4 = document.querySelector("#btn-abrir-modal4");
+const btnCerrarModal4 = document.querySelector("#btn-cerrar-modal4");
+const modal4 = document.querySelector("#modal4");
+
+btnAbrirModal4.addEventListener("click",()=>{modal4.showModal();
+
+})
+btnCerrarModal4.addEventListener("click",()=>{modal4.close();
+    
+})
