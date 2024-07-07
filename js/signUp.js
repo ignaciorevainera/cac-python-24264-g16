4beb47bf857fd023a905bef537e5bfe1678d9684
Ignacio Revainera <ignacio.revainera@hotmail.com>
2024-07-07 14:21:27 -0300

    Refactorización de código

--- js/signUp.js
+++ js/signUp.js
@@ -6,28 +6,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	form.addEventListener("submit", (event) => {
 		if (password.value !== confirmPassword.value) {
-			event.preventDefault();		//Se cancela el submit del form.
+			event.preventDefault(); //Se cancela el submit del form.
 			errorMessage.classList.remove("hidden");
-		} else { 
+		} else {
 			errorMessage.classList.add("hidden");
-			
+
 			//Data en orden => fetch a la API
 
-			let user_info={
-				'email':form.getElementById('email').value,
-				'username':form.getElementById('username').value,
-				'password':password.value
-			}
+			let user_info = {
+				email: form.getElementById("email").value,
+				username: form.getElementById("username").value,
+				password: password.value,
+			};
 
-			fetchData("http://127.0.0.1:5000/register/",
+			fetchData(
+				"http://127.0.0.1:5000/register/",
 				"POST",
 				(data) => {
 					form.reset();
-					window.location.replace("../index.html") //A modificar a ventana de user
+					window.location.replace("../index.html"); //A modificar a ventana de user
 				},
 				user_info
 			);
 		}
 	});
 });
-
--- js/singIn.js
+++ js/singIn.js
@@ -6,20 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	form.addEventListener("submit", (event) => {
 		if (password.value !== confirmPassword.value) {
-			event.preventDefault();		//Se cancela el submit del form.
+			event.preventDefault(); //Se cancela el submit del form.
 			errorMessage.classList.remove("hidden");
-		} else { 
+		} else {
 			errorMessage.classList.add("hidden");
-			
+
 			//Data en orden => fetch a la API.
 			//Hay que ver como se efectua la validación;con el error code..? TODO
 
-
-			fetchData("http://127.0.0.1:5000/login/",
+			fetchData(
+				"http://127.0.0.1:5000/login/",
 				"POST",
 				(data) => {
 					form.reset();
-					window.location.replace("../index.html") //A modificar a ventana de user
+					window.location.replace("../index.html"); //A modificar a ventana de user
 				},
 				user_info
 			);
